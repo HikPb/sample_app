f7f44a630df97f6ddc3a058599d73dc5ca402e4b
HikPb <p.ngocba@gmail.com>
2019-12-22 18:56:24 +0700

    config environment product

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://murmuring-island-17028.herokuapp.com'
+  host = 'https://vast-sea-13843.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
